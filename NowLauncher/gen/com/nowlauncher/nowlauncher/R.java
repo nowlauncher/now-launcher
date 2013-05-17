d11e7be1373538296bb349e53dc1c2f73daca050
Edoardo Debenedetti <dedeswim@gmail.com>
2013-05-17 20:05:50 +0200

    Mad some changes to Gmail icon, added Calculator, Google+, Settings

--- NowLauncher/gen/com/nowlauncher/nowlauncher/R.java
+++ NowLauncher/gen/com/nowlauncher/nowlauncher/R.java
@@ -42,8 +42,7 @@ public final class R {
     public static final class drawable {
         public static final int googlenow=0x7f020000;
         public static final int ic_launcher=0x7f020001;
-        public static final int pached=0x7f020002;
-        public static final int sun=0x7f020003;
+        public static final int sun=0x7f020002;
     }
     public static final class id {
         public static final int Border1=0x7f090017;
