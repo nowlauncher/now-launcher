fcf513d06de1dcdfe22ff821ce98702c8cb0d43f
Mm7 <marcopc3@msn.com>
2013-05-19 19:35:27 +0200

    added_somethings

---
+++ NowLauncher/bin/NowLauncher.ap_.d
@@ -0,0 +1,26 @@
+/home/marco/workspace/NowLauncher/bin/NowLauncher.ap_ \
+ : /home/marco/workspace/NowLauncher/res/drawable-mdpi/ic_launcher.png \
+/home/marco/workspace/NowLauncher/res/drawable-hdpi/googlenow.jpg \
+/home/marco/workspace/NowLauncher/res/drawable-hdpi/pach.png \
+/home/marco/workspace/NowLauncher/res/drawable-hdpi/sun.jpg \
+/home/marco/workspace/NowLauncher/res/drawable-hdpi/ic_launcher.png \
+/home/marco/workspace/NowLauncher/res/drawable-xxhdpi/ic_launcher.png \
+/home/marco/workspace/NowLauncher/res/values/dimens.xml \
+/home/marco/workspace/NowLauncher/res/values/color.xml \
+/home/marco/workspace/NowLauncher/res/values/styles.xml \
+/home/marco/workspace/NowLauncher/res/values/strings.xml \
+/home/marco/workspace/NowLauncher/res/values-sw600dp/dimens.xml \
+/home/marco/workspace/NowLauncher/res/values-sw720dp-land/dimens.xml \
+/home/marco/workspace/NowLauncher/res/menu/main.xml \
+/home/marco/workspace/NowLauncher/res/drawable-xhdpi/ic_launcher.png \
+/home/marco/workspace/NowLauncher/res/layout/drawer.xml \
+/home/marco/workspace/NowLauncher/res/layout/weather_widget.xml \
+/home/marco/workspace/NowLauncher/res/layout/activity_main.xml \
+/home/marco/workspace/NowLauncher/res/layout/raw.xml \
+/home/marco/workspace/NowLauncher/res/layout/dock.xml \
+/home/marco/workspace/NowLauncher/bin/res/drawable-mdpi/ic_launcher.png \
+/home/marco/workspace/NowLauncher/bin/res/drawable-hdpi/pach.png \
+/home/marco/workspace/NowLauncher/bin/res/drawable-hdpi/ic_launcher.png \
+/home/marco/workspace/NowLauncher/bin/res/drawable-xxhdpi/ic_launcher.png \
+/home/marco/workspace/NowLauncher/bin/res/drawable-xhdpi/ic_launcher.png \
+/home/marco/workspace/NowLauncher/bin/AndroidManifest.xml \
