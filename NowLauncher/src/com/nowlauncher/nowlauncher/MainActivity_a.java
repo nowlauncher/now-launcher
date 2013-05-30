aa51b3504e1edca9e2bd038919b63a0612d1e85a
dema121 <andrea.dematte@gmail.com>
2013-05-30 17:39:30 +0200

    Swype drawer-bar function, animation function
    Fixed and improved drawer-bar scroll algorithm.
    Introduced animations in the drawer-bar.

--- NowLauncher/src/com/nowlauncher/nowlauncher/MainActivity_a.java
+++ NowLauncher/src/com/nowlauncher/nowlauncher/MainActivity_a.java
@@ -18,6 +18,9 @@ import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
@@ -27,6 +30,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
@@ -64,6 +68,13 @@ public class MainActivity extends Activity {
 	
 	// Array of all application in the device
 	private ArrayList<AppInfo> mApplications;
+
+    //Variabili per il controllo della selezione della barra e del touch
+    public boolean checkbarpressed = (boolean) false;
+    public int y;
+    public int yiniziale=0;
+    public boolean yinizialebool = (boolean) false;
+
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -90,16 +101,27 @@ public class MainActivity extends Activity {
 		
 		dropdownbar2 = (ImageView) findViewById(R.id.DropDownBar2);
 		rootlayout = (RelativeLayout) findViewById(R.id.rootLayout);
-		
-		/**
-		* EN
-		* Load array of applications and set up drawer
-		*
-		* IT
-		* Carica la lista delle applicazioni e carica il drawer
-		*/
-		
-		LoadApplication();
+        dropdownbar2.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    //imposta il booleano su true quando la barra è premuta
+                    checkbarpressed=true;
+                }
+                return false;
+            }
+        });
+
+
+                /**
+                 * EN
+                 * Load array of applications and set up drawer
+                 *
+                 * IT
+                 * Carica la lista delle applicazioni e carica il drawer
+                 */
+
+                LoadApplication();
 		CreateViews();
 	          
 	}
@@ -211,15 +233,20 @@ public class MainActivity extends Activity {
 		* Ottiene il valore di y e calcola se il touch è nella zona di tolleranza della drop down bar
 		*/
 		
-		int y = (int) event.getY();
-            
-		if ((y >= (dropdownbar2.getTop() - TOLLERANCE_TOP ) ) || ( y <= (dropdownbar2.getBottom() + TOLLERANCE_BOTTOM ))) {
-			
+	    y = (int) event.getY();
+
+        if (checkbarpressed==true) {
+            if (yinizialebool==false) {
+                //imposta la coordinata y iniziale per il successivo controllo dello "spostamento" del dito
+                yiniziale=y;
+                yinizialebool=true;
+            }
+
 			RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-           
+
 			statusBarOffset = dm.heightPixels - rootlayout.getHeight();
-            
-			
+
+
 			/**
 			* EN
 			* If the touch is over the minimal bar position before compression set it in the minimal position
@@ -227,7 +254,9 @@ public class MainActivity extends Activity {
 			* IT
 			* Se il touch è oltre la posizione minima della barra prima di ridursi di altezza la barra viene posizionata nel punto minimo
 			*/
-			
+
+            //Qui non ho capito molto bene l'algoritmo ma funziona... quindi! :D (dema121)
+
 			if ((rootlayout.getHeight() - (y - statusBarOffset)) <= dropdownbar2.getHeight())
 				params.topMargin = rootlayout.getHeight() - dropdownbar2.getHeight();
 			else if ((y - statusBarOffset) <= dropdownbar2.getHeight())            
@@ -235,12 +264,64 @@ public class MainActivity extends Activity {
 			else params.topMargin = y - statusBarOffset - dropdownbar2.getHeight()/2;
             
 			dropdownbar2.setLayoutParams(params);
-        
+
 		}
-        
-		return false;
-        
-	}
+        if(event.getAction() == MotionEvent.ACTION_UP) {
+            yinizialebool=false;
+            if (checkbarpressed==true) {
+
+                final RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                if (yiniziale>y){
+                    params.topMargin = 0;
+                }
+                else {
+                    params.topMargin = rootlayout.getHeight()-dropdownbar2.getHeight();
+                }
+
+
+                int originalPos[] = new int[2];
+                dropdownbar2.getLocationOnScreen( originalPos );
+
+                Animation animation = new TranslateAnimation(0, 0, 0, 0-(dropdownbar2.getTop()-params.topMargin));
+
+                // il costruttore prevede 4 int, x iniziale, x finale, y iniziale e y finale. Tutte le coordinate sono relative al punto in cui si trova la view prima dell'animazione (es. 0,0,0,10 sposterà la view di 10dp in alto)
+
+                // A better form is:
+                // TraslateAnimation animation = new TranslateAnimation(0, 0, 0, -originalPos[1]);
+                // or
+                // TraslateAnimation animation = new TranslateAnimation(0, 0, 0, -(dropdownbar2.geTop()));
+
+                animation.setDuration(300);
+                animation.setFillAfter(false); // <-- fa in modo che a fine animazione la view rimanga nel posto e non ritorni al posto iniziale
+                dropdownbar2.startAnimation(animation);
+                animation.setInterpolator(new AccelerateInterpolator(1));
+
+                animation.setAnimationListener(new Animation.AnimationListener(){
+
+
+
+                    @Override
+                    public void onAnimationStart(Animation anim) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation anim) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation anim) {
+
+                        dropdownbar2.setLayoutParams(params);
+
+                    }
+                });
+            checkbarpressed=false;
+            }
+		}
+        return false;
+    }
         
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
